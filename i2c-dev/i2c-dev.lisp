fc0f4b0d696c0ba99696ae7fdd2c1d51ce9c41b7
ast <kon.bass@gmail.com>
2026-02-26 00:45:31 +0900

    Implement i2c-dev wrapper directly on Common Lisp

--- i2c-dev/i2c-dev.lisp
+++ i2c-dev/i2c-dev.lisp
@@ -12,12 +12,105 @@
     (setf *dev*
           (cffi:foreign-string-alloc device-path)))
 
-(defun i2c-read ()
-    0)
+(defun i2c-read (addr read-buf read-buf-len)
+    (cffi:with-foreign-objects ((msg '(:struct i2c-msg))
+                                (packets '(:struct i2c-rdwr-ioctl-data)))
+        (setf (cffi:foreign-slot-value msg '(:struct i2c-msg) 'addr) addr
+              (cffi:foreign-slot-value msg '(:struct i2c-msg) 'flags) I2C-M-RD
+              (cffi:foreign-slot-value msg '(:struct i2c-msg) 'buf) read-buf
+              (cffi:foreign-slot-value msg '(:struct i2c-msg) 'len) read-buf-len)
+        (setf (cffi:foreign-slot-value packets '(:struct i2c-rdwr-ioctl-data) 'msgs) msg
+              (cffi:foreign-slot-value packets '(:struct i2c-rdwr-ioctl-data) 'nmsgs) 1)
+        (let ((fd (cffi:foreign-funcall "open"
+                                        (:pointer :char) *dev*
+                                        :int O-RDWR
+                                        :int)))
+            (if (< fd 0)
+                (progn
+                    (format t "fail open()")
+                    -1) ; ERROR
+                (if (< (cffi:foreign-funcall "ioctl"
+                                             :int fd
+                                             :ulong I2C-RDWR
+                                             (:pointer (:struct i2c-rdwr-ioctl-data)) packets
+                                             :int)
+                       0)
+                    (progn
+                        (format t "fail ioctl()")
+                        -1) ; ERROR
+                    (progn (cffi:foreign-funcall "close"
+                                          :int fd
+                                          :int)
+                        0))))))
 
-(defun i2c-read-register ()
-    0)
+(defun i2c-read-register (addr write-buf write-buf-len read-buf read-buf-len)
+    (cffi:with-foreign-objects ((msgs '(:struct i2c-msg) 2)
+                                (packets '(:struct i2c-rdwr-ioctl-data)))
+        ; write buf
+        (let ((msg (cffi:mem-aref msgs '(:struct i2c-msg) 0)))
+            (setf (cffi:foreign-slot-value msg '(:struct i2c-msg) 'addr) addr
+                  (cffi:foreign-slot-value msg '(:struct i2c-msg) 'flags) 0
+                  (cffi:foreign-slot-value msg '(:struct i2c-msg) 'buf) write-buf
+                  (cffi:foreign-slot-value msg '(:struct i2c-msg) 'len) write-buf-len))
+        ; read buf
+        (let ((msg (cffi:mem-aref msgs '(:struct i2c-msg) 1)))
+            (setf (cffi:foreign-slot-value msg '(:struct i2c-msg) 'addr) addr
+                  (cffi:foreign-slot-value msg '(:struct i2c-msg) 'flags) I2C-M-RD
+                  (cffi:foreign-slot-value msg '(:struct i2c-msg) 'buf) read-buf
+                  (cffi:foreign-slot-value msg '(:struct i2c-msg) 'len) read-buf-len))
+        ; Set packets
+        (setf (cffi:foreign-slot-value packets '(:struct i2c-rdwr-ioctl-data) 'msgs) msgs
+              (cffi:foreign-slot-value packets '(:struct i2c-rdwr-ioctl-data) 'nmsgs) 2)
+        (let ((fd (cffi:foreign-funcall "open"
+                                        (:pointer :char) *dev*
+                                        :int O-RDWR
+                                        :int)))
+            (if (< fd 0)
+                (progn
+                    (format t "fail open()")
+                    -1) ; ERROR
+                (if (< (cffi:foreign-funcall "ioctl"
+                                             :int fd
+                                             :ulong I2C-RDWR
+                                             (:pointer (:struct i2c-rdwr-ioctl-data)) packets
+                                             :int)
+                       0)
+                    (progn
+                        (format t "fail ioctl()")
+                        -1) ; ERROR
+                    (progn (cffi:foreign-funcall "close"
+                                          :int fd
+                                          :int)
+                        0))))))
 
-(defun i2c-write ()
-    0)
+(defun i2c-write (addr write-buf write-buf-len)
+    (cffi:with-foreign-objects ((msg '(:struct i2c-msg))
+                                (packets '(:struct i2c-rdwr-ioctl-data)))
+        (setf (cffi:foreign-slot-value msg '(:struct i2c-msg) 'addr) addr
+              (cffi:foreign-slot-value msg '(:struct i2c-msg) 'flags) 0
+              (cffi:foreign-slot-value msg '(:struct i2c-msg) 'buf) write-buf
+              (cffi:foreign-slot-value msg '(:struct i2c-msg) 'len) write-buf-len)
+        (setf (cffi:foreign-slot-value packets '(:struct i2c-rdwr-ioctl-data) 'msgs) msg
+              (cffi:foreign-slot-value packets '(:struct i2c-rdwr-ioctl-data) 'nmsgs) 1)
+        (let ((fd (cffi:foreign-funcall "open"
+                                        (:pointer :char) *dev*
+                                        :int O-RDWR
+                                        :int)))
+            (if (< fd 0)
+                (progn
+                    (format t "fail open()")
+                    -1) ; ERROR
+                (if (< (cffi:foreign-funcall "ioctl"
+                                             :int fd
+                                             :ulong I2C-RDWR
+                                             (:pointer (:struct i2c-rdwr-ioctl-data)) packets
+                                             :int)
+                       0)
+                    (progn
+                        (format t "fail ioctl()")
+                        -1) ; ERROR
+                    (progn (cffi:foreign-funcall "close"
+                                          :int fd
+                                          :int)
+                        0))))))
 
